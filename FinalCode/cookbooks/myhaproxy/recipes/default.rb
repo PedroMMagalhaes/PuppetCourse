797b04885b12d8c09f06241870b7243fdd4ec25f
Pedro Magalhães <2150672@my.ipleiria.pt>
2020-02-12 12:48:18 +0000

    Add files via upload

---
+++ FinalCode/cookbooks/myhaproxy/recipes/default.rb
@@ -0,0 +1,21 @@
+#
+# Cookbook Name:: myhaproxy
+# Recipe:: default
+#
+# Copyright (c) 2017 The Authors, All Rights Reserved.
+
+node.default['haproxy']['members'] = [
+{
+    'hostname' => 'teste1',
+    'ipaddress' => '3.19.227.7',
+    'port' => 80,
+    'ssl_port' => 80
+  }, {
+    'hostname' => 'teste2',
+    'ipaddress' => '3.15.152.140',
+    'port' => 80,
+    'ssl_port' => 80
+}]
+
+include_recipe 'haproxy::default'
+
