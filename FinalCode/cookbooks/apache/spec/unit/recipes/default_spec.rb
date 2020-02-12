405a661390e4d6556c2ff917a032989d60664c49
Pedro Magalhães <pedromagtaem@gmail.com>
2020-02-12 12:53:10 +0000

    Update default_spec.rb

--- FinalCode/cookbooks/apache/spec/unit/recipes/default_spec.rb
+++ FinalCode/cookbooks/apache/spec/unit/recipes/default_spec.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: apache
 # Spec:: default
 #
-# Copyright (c) 2017 The Authors, All Rights Reserved.
+# Copyright (c) 2020 The Authors, All Rights Reserved.
 
 require 'spec_helper'
 
